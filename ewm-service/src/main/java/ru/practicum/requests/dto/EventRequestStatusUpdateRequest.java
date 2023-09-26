2a7877e5e03ac9cfc76451453fe448096462a4cd
Волынка <olganv1707@yandex.ru>
2023-09-27 01:14:11 +0300

    code style

--- ewm-service/src/main/java/ru/practicum/requests/dto/EventRequestStatusUpdateRequest.java
+++ ewm-service/src/main/java/ru/practicum/requests/dto/EventRequestStatusUpdateRequest.java
@@ -12,6 +12,7 @@ import java.util.Set;
 public class EventRequestStatusUpdateRequest {
     private Set<Long> requestIds;
     private EventRequestStatus status;
+
     public enum EventRequestStatus {
         CONFIRMED,
         REJECTED
