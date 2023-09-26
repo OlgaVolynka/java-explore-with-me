0bc88561a03559c70ba4574dfb4afb727d20e7ed
Волынка <olganv1707@yandex.ru>
2023-09-27 01:50:52 +0300

    fix stats service

--- stats-server/src/main/java/ru/practicum/StatsService.java
+++ stats-server/src/main/java/ru/practicum/StatsService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.exception.DataNotCorrectException;
+import ru.practicum.exception.DataNotFoundException;
 import ru.practicum.model.EndpointHit;
 import ru.practicum.model.ViewStats;
 import ru.practicum.model.dto.GetEndpointHitDto;
@@ -27,14 +28,13 @@ public class StatsService {
 
     public List<ViewStats> getHit(LocalDateTime start, LocalDateTime end, String[] uri, Boolean unique) {
 
-        if (start == null) start = LocalDateTime.now();
+        if (start == null || end == null) throw new DataNotFoundException("Start or and not present");
         if (start.isAfter(end)) throw new DataNotCorrectException("Start should be before and");
 
         if (uri == null) return statsRepository.getStatsNotUri(start, end);
-        else if (unique == true) return statsRepository.getStatsUnique(start, end, uri);
+        else if (unique) return statsRepository.getStatsUnique(start, end, uri);
         else {
-            List<ViewStats> v = statsRepository.getStatsNotUnique(start, end, uri);
-            return v;
+            return statsRepository.getStatsNotUnique(start, end, uri);
         }
 
     }
--- stats-server/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ stats-server/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
 import java.util.Map;
@@ -20,6 +22,16 @@ public class ErrorHandler {
                 .message(e.getMessage())
                 .reason("Request is not correctly")
                 .status(HttpStatus.BAD_REQUEST)
+                .timestamp(LocalDateTime.now()).build();
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiError handleInternalServerErrorException(final DataNotFoundException e) {
+
+        return new ApiError.ApiErrorBuilder()
+                .message(e.getMessage())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .timestamp(LocalDateTime.now()).build();
     }
 
