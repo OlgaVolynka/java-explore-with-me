6354e10b8690469c9f1920aec06cc92e975a70f6
Волынка <olganv1707@yandex.ru>
2023-09-27 01:52:42 +0300

    code style

--- stats-server/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ stats-server/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -5,8 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
 import java.util.Map;
